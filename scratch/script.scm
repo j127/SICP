91320d7529d5f13a2979779e23898b53e7f65275
J. Cohen <hostelmarketing@gmail.com>
2020-06-27 13:59:14 -0700

    Update scratch and fix char

--- scratch/script.scm
+++ scratch/script.scm
@@ -1,10 +1,17 @@
 ; $ mit-scheme --quiet < script.scm
 ; (display  (+ 2 2))
 
-(define result (+ (* 3
-      (+ (* 2 4)
-         (+ 3 5)))
-   (+ (- 10 7)
-      6)))
+; (define result (+ (* 3
+;       (+ (* 2 4)
+;          (+ 3 5)))
+;    (+ (- 10 7)
+;       6)))
 
-(display result)
+(define result (+ 2 2))
+
+(define p
+  (lambda (val)
+    (display val)
+    (display "\n")))
+
+(p result)
